bef67b7d737ec4ff38cf98ccb3b2079285baa45c
HuynhTonyy <107403128+HuynhTonyy@users.noreply.github.com>
2025-09-15 20:23:22 +0700

    Refactor customer prefabs and add Customer scripts
    Removed individual customer prefabs (Barbarian, Knight, Mage) and replaced them with a unified Customer prefab. Added Customer.cs and MovementNavMesh.cs scripts to support new customer logic. Updated furniture prefabs and scene assets to integrate the new customer system.

--- Assets/Scripts/Manager/Customer Manager.cs
+++ Assets/Scripts/Manager/Customer Manager.cs
@@ -1,9 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private PoolType poolType;
+
     private CustomerManager Instance;
-    private void Awake() {
+    private void Awake()
+    {
         if (!Instance)
         {
             Instance = this;
@@ -14,4 +20,21 @@ public class CustomerManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    void Start()
+    {
+        Spawn(1);
+    }
+    private void Spawn(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            EventManager.Instance.SpawnObject(poolType, Vector3.zero, Quaternion.identity, spawnPoint);
+        }
+    }
+}
+public struct CustomerDemain
+{
+    private GameObject customerObj;
+    private RecipeSO recipeSO;
+    
 }
